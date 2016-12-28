758dff4c096096a49fcafce1e4cbde0d6bc35509
Sönke Hahn <soenkehahn@gmail.com>
2016-12-28 00:11:13 -0500

    misc

--- src/Main.elm
+++ src/Main.elm
@@ -4,9 +4,12 @@ import LevelSequence exposing (..)
 import RunLevel exposing (..)
 import Html exposing (program)
 import Utils exposing (..)
-import Random exposing (..)
+import Level.Model
 
+type alias Model =
+  LevelSequence.Model Level.Model.Level Level.Model.Model RunLevel.Msg
 
+main : Program Never Model (LevelSequence.Msg RunLevel.Msg)
 main =
     let
         (Component c) =
